8845b633cc1ded68708592702ca2015c99f5ba4c
Ehor-developer <123355005+Ehor-developer@users.noreply.github.com>
2023-02-10 18:09:22 +0900

    Add files via upload

---
+++ js/get.js
@@ -0,0 +1,36 @@
+//パラメータを取得する処理
+function parm(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+//四捨五入して、緯度と経度を表示
+//document.getElementById("ido").innerHTML = Math.round(parm('ido') * 1000) / 1000;
+//document.getElementById("keido").innerHTML = Math.round(parm('keido') * 1000) / 1000;
+//document.getElementById("map").setAttribute("href", "https://www.google.co.jp/maps/@" + parm('ido') + "," + parm('keido') + ",19z");
+
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var map = L.map('maps', {
+    center: [parm('ido'), parm('keido')],
+    zoom: 8,
+    layers: [osm]
+})
+
+function customIcon(latLng, size, type) {
+    var customIcon = L.divIcon({
+        html: `<img class="pin" src="icon/icon.png">`,
+        className: type,
+        iconSize: [size,size],
+    });
+    var marker = L.marker(latLng, {icon: customIcon});
+    return marker;
+}
+
+customIcon([parm('ido'), parm('keido')], '90', 100, 'circle').addTo(map);
